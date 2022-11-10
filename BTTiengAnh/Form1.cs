55fcddd6638e6736bf6645f4b33c0173d29e9472
Oanhha213 <75170190+Oanhha213@users.noreply.github.com>
2022-11-10 20:56:39 +0700

    commitTest1

---
+++ BTTiengAnh/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTTiengAnh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void đTBài1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileCH = "E:\\LTTQ\\Cauhoi\\Cauhoi1.txt";
+            string fileDA = "E:\\LTTQ\\Cauhoi\\Cauhoi1DapAn.txt";
+            FillWord fw = new FillWord(fileCH, fileDA);
+            fw.ShowDialog();
+        }
+
+        private void đTBài2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileCH = "E:\\LTTQ\\Cauhoi\\Cauhoi2.txt";
+            string fileDA = "E:\\LTTQ\\Cauhoi\\Cauhoi2DapAn.txt";
+            FillWord fw = new FillWord(fileCH, fileDA);
+            fw.ShowDialog();
+        }
+    }
+}
---
+++ BTTiengAnh/QLFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BTTiengAnh
+{
+    internal class QLFile
+    {
+        public string readCH(string tentep)
+        {
+            string s = "";
+            StreamReader sr = new StreamReader(tentep);
+            s = sr.ReadToEnd();
+            sr.Close();
+            return s;
+        }
+        public List<string> readDA(string tentep)
+        {
+            List<string> list = new List<string>();
+            string s = "";
+            StreamReader sr = new StreamReader(tentep);
+            while((s = sr.ReadLine()) != null)
+            {
+                list.Add(s);
+            }
+            sr.Close();
+            return list;
+        }
+    }
+}
